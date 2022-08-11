563f9a09165ca265ba70f07d5004af4a8347a446
Pavel <m83819@gmail.com>
2022-08-11 17:25:33 +0700

    Task004_DivSame

---
+++ Task004_DivSame/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+4. Напишите программу, которая принимает на вход 
+число и проверяет, кратно ли оно одновременно 7 и 
+23.
+14 -> нет 
+46 -> нет 
+161 -> да
+
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+bool DivSame(int input)
+{
+    if((input%7==0)&&(input%23==0)) {return true;}
+    return false;
+
+}
+
+int num1=Prompt("Введите число:");
+
+if(DivSame(num1)) 
+{
+    Console.WriteLine ($"Ваше число {num1} кратно 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine ($"Ваше число {num1} НЕ кратно 7 и 23 одновременно");
+}
+
