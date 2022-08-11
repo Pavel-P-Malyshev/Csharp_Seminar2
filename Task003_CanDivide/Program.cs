a3925ef62a3ab19a51597db8d601623574231868
Pavel <m83819@gmail.com>
2022-08-11 17:13:49 +0700

    Task003_CanDivide

---
+++ Task003_CanDivide/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+3. Напишите программу, которая будет принимать на 
+вход два числа и выводить, является ли второе число 
+кратным первому. Если число 2 не кратно числу 1, то 
+программа выводит остаток от деление.
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+int num1=Prompt("Введите число1:");
+int num2=Prompt("Введите число2:");
+if(num1%num2>0) {
+    Console.WriteLine ($"Остаток от деления {num1}/{num2} равен:{num1%num2}");
+}
