d8404f74eeb27b2133295bc2c3aeb40362756b9f
Pavel <m83819@gmail.com>
2022-08-11 16:42:28 +0700

    Task002_MostDigit

---
+++ Task002_MostDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿int MostVal()
+{
+    int randomval=new Random().Next(10,99);
+    Console.WriteLine($"Generated value is:{randomval}");
+
+    if((randomval/10)>(randomval%10)) 
+    {
+        return randomval/10;
+    }
+    else
+    {
+        return randomval%10;
+    }
+    return 0;
+}
+
+
+
+int A = MostVal();
+
+Console.WriteLine($"Largest digit is {A}");
