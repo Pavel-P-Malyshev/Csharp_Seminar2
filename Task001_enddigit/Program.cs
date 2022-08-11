a935060a43cc910e9caa22ae65416f3abb8e3c93
Pavel <m83819@gmail.com>
2022-08-11 16:06:42 +0700

    Task001_enddigit

---
+++ Task001_enddigit/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int EndDigit(int value)
+{
+        
+    return value%10;
+}
+Console.WriteLine("Enter you 3 digits number:");
+int A = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+while(A>999||A<100)
+    { 
+        Console.WriteLine("Your number has not 3 dogits, enter another:");
+        A = int.Parse(Console.ReadLine());
+    }
+Console.WriteLine($"Last digit of your number is: {EndDigit(A)}");
