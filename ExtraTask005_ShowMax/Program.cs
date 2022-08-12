f721ad8ec9ec9c99c7ed02398cdc8adbd4a520a5
Pavel <m83819@gmail.com>
2022-08-12 09:45:33 +0700

    ExtraTask005_ShowMaxVer2

--- ExtraTask005_ShowMax/Program.cs
+++ ExtraTask005_ShowMax/Program.cs
@@ -18,34 +18,44 @@ int ShowMax()
 {
     int randomval=new Random().Next(10,100000);
     Console.WriteLine($"Generated value is:{randomval}");
+    
     List<int> Digits=new List<int>();
     List<int> Quots=new List<int>();
-
     Digits.Add(randomval%10);
     Quots.Add(randomval/10);
-    //Console.WriteLine(Digits[0]);
-    //Console.WriteLine(Quots[0]);
-    //Console.WriteLine(8%10);
-    //Console.WriteLine(8/10);
-
-   int index=1;
-   int max=Digits[0];
+    int index=1;
+    int max=Digits[0];
 
     while(Quots[index-1]>0)
     {
         Quots.Add(Quots[index-1]/10);
         Digits.Add(Quots[index-1]%10);
-        //Console.WriteLine($" this is while:{Digits[index]}");
+       
         if(Digits[index]>max) {max=Digits[index];}
-        //Console.WriteLine(index);
-        //Console.WriteLine($" this is max in while:{max}");
-
+       
         index++;
     }
+
+    //второй вариант цикла - просто делим на 10 в цикле пока частное не будет меньше нуля
+    //и сразу записываем в макс
+    /*
+     int max=0;
+     while(randomval>0)
+     {
+        int a=randomval%10;
+        if (max<a){max=a;}
+        randomval=randomval/10
+     }
+
+    */
+
+
+
 
     return max;
 }
 
 int result=ShowMax();
 Console.WriteLine($"maximum digit of random number is:{result}");
+
 
