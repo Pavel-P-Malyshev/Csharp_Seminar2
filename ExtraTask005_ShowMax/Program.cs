2da54c82b37994e630031ff144877771a047dc4e
Pavel <m83819@gmail.com>
2022-08-12 09:39:13 +0700

    ExtraTask005_ShowMax

---
+++ ExtraTask005_ShowMax/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+2.1. Напишите программу, которая выводит 
+случайное число из отрезка [10, 9999] и 
+показывает наибольшую цифру числа.
+7812 -> 8 
+1213-> 3 
+845 -> 8
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+int ShowMax()
+{
+    int randomval=new Random().Next(10,100000);
+    Console.WriteLine($"Generated value is:{randomval}");
+    List<int> Digits=new List<int>();
+    List<int> Quots=new List<int>();
+
+    Digits.Add(randomval%10);
+    Quots.Add(randomval/10);
+    //Console.WriteLine(Digits[0]);
+    //Console.WriteLine(Quots[0]);
+    //Console.WriteLine(8%10);
+    //Console.WriteLine(8/10);
+
+   int index=1;
+   int max=Digits[0];
+
+    while(Quots[index-1]>0)
+    {
+        Quots.Add(Quots[index-1]/10);
+        Digits.Add(Quots[index-1]%10);
+        //Console.WriteLine($" this is while:{Digits[index]}");
+        if(Digits[index]>max) {max=Digits[index];}
+        //Console.WriteLine(index);
+        //Console.WriteLine($" this is max in while:{max}");
+
+        index++;
+    }
+
+    return max;
+}
+
+int result=ShowMax();
+Console.WriteLine($"maximum digit of random number is:{result}");
+
